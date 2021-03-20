03fe5f16b2f8ea70928f6f74936dc8d14d9331a8
Ivan6274 <isivanov2011@gmail.com>
2021-03-20 09:14:19 +0300

    Add Users Info

--- src/test/java/ru/netology/Patterns.java
+++ src/test/java/ru/netology/Patterns.java
@@ -1,34 +1,23 @@
 package ru.netology;
 
 import com.codeborne.selenide.Condition;
-import com.github.javafaker.Faker;
-
-
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-
-
-import java.text.DecimalFormat;
 import java.time.Duration;
 import java.time.LocalDate;
-import java.util.Locale;
-
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
-
 public class Patterns {
-
-
-    private final Faker faker = new Faker(new Locale("ru"));
-
 
     @Test
     void cardDeliveryWithCalendar() {
+
         open("http://localhost:9999");
-        $("[data-test-id='city'] .input__control").setValue(faker.address().cityName());
+        $("[data-test-id='city'] .input__control").setValue(new UsersInfo().faker.address().cityName());
 
         $(By.cssSelector(("[data-test-id=\"date\"]  .input__box"))).click();
         int addDay = 7;
@@ -40,12 +29,12 @@ public class Patterns {
             $(By.cssSelector(".calendar__title [class='calendar__arrow calendar__arrow_direction_right']")).click();
             $(byText(String.valueOf(needDay.getDayOfMonth()))).click();
         }
-        $("[data-test-id='name'] .input__control").setValue(faker.name().fullName());
-        $("[data-test-id=\"phone\"] .input__control").setValue(faker.phoneNumber().phoneNumber());
+        $("[data-test-id='name'] .input__control").setValue(new UsersInfo().faker.name().fullName());
+        $("[data-test-id=\"phone\"] .input__control").setValue(new UsersInfo().faker.phoneNumber().phoneNumber());
         $("[data-test-id=\"agreement\"] .checkbox__box").click();
         $("[role=\"button\"] .button__content").click();
         $(withText("Успешно")).shouldBe(Condition.visible, Duration.ofSeconds(25));
-        $(withText(new DecimalFormat("00").format(needDay.getDayOfMonth()) + "." + new DecimalFormat("00").format(needDay.getMonthValue()) + "." + needDay.getYear())).shouldBe(Condition.visible, Duration.ofSeconds(25));
+        $(withText(needDay.format(DateTimeFormatter.ofPattern("dd.MM.yyyy")))).shouldBe(Condition.visible, Duration.ofSeconds(25));
         $(By.cssSelector(("[data-test-id=\"date\"]  .input__box"))).click();
         int addDay1 = 10;
         LocalDate newDay = today.plusDays(addDay1);
@@ -54,11 +43,10 @@ public class Patterns {
         } else {
             $(By.cssSelector(".calendar__title [class='calendar__arrow calendar__arrow_direction_right']")).click();
             $(byText(String.valueOf(newDay.getDayOfMonth()))).click();
-
         }
         $("[role=\"button\"] .button__content").click();
         $(withText("Перепланировать")).shouldBe(Condition.visible, Duration.ofSeconds(25)).click();
-        $(withText(new DecimalFormat("00").format(newDay.getDayOfMonth()) + "." + new DecimalFormat("00").format(newDay.getMonthValue()) + "." + newDay.getYear())).shouldBe(Condition.visible, Duration.ofSeconds(25));
+        $(withText(newDay.format(DateTimeFormatter.ofPattern("dd.MM.yyyy")))).shouldBe(Condition.visible, Duration.ofSeconds(25));
 
     }
 }
--- src/test/java/ru/netology/UsersInfo.java
+++ src/test/java/ru/netology/UsersInfo.java
@@ -1,12 +1,16 @@
 package ru.netology;
 
+import com.github.javafaker.Faker;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Locale;
 
 @Data
 @RequiredArgsConstructor
 
 public class UsersInfo {
+    Faker faker = new Faker(new Locale("ru"));
     private String name;
     private String phone;
     private String city;
